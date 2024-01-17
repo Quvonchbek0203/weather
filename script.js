6c9a63d5624c466a327447fa92b3b59b3ef0ddf3
Quvonchbek0203 <150164830+Quvonchbek0203@users.noreply.github.com>
2024-01-17 20:17:46 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// GET => olmoq 
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then((response) =>  response.json())
+// .then((data)=>console.log(data))
+
+// POST => yuklash , yuklamoq
+// fetch("https://jsonplaceholder.typicode.com/posts",{
+//     method:"POST",
+//     headers:{
+//         "Content-Type":"application/json"
+//     },
+//     body:JSON.stringify({name:"Umid"})
+// })
+
+//     .then((response)=>response.json())
+//     .then((data)=>console.log(data))
+
+
+// DELETE =>  o'chirish
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1" ,{
+//     method:"DELETE",
+// })
+//     .then(()=>console.log("DELETE SUCCESSFULL"))
